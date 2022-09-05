742f9ce62b10e566b84a988fe6278ef85bde5bb9
nichitacebotari0 <nichitacebotari0@gmail.com>
2022-09-05 15:43:59 +0100

    storage account name lowercase

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "prodstaFangsBuilder"
+  name                     = "prodstafangsbuilder"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_kind             = "StorageV2"
