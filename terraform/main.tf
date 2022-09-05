215d34227706b8a1326f72992d044d05d2b28348
nichitacebotari0 <nichitacebotari0@gmail.com>
2022-09-05 15:37:16 +0100

    terraform fmt, yaml format

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,29 +15,28 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "~>3.0"
   features {}
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name = "FangsBuilder"
+  name     = "FangsBuilder"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "prodstaFangsBuilder"
-  resource_group_name      = data.azurerm_resource_group.resource_group.name
-  location                 = data.azurerm_resource_group.resource_group.location
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-    static_website {
+  static_website {
     index_document = "index.html"
   }
 }
 
-output StorageAccountName {
+output "StorageAccountName" {
   value = azurerm_storage_account.storage_account.name
 }
 
