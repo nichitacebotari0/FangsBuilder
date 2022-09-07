9b3efca4280cbef6ecb0830ec383fe24df1c7af8
nichitacebotari0 <nichitacebotari0@gmail.com>
2022-09-07 21:48:42 +0100

    now generating hero json using OCR

--- src/dev/CreateJsons.ps1
+++ src/dev/CreateJsons.ps1
@@ -45,69 +45,50 @@ function AutoRenameHeroImages($directory) {
 }
 
 
-# OCR cus im tired of writing by hand after 4 heroes
-Find-Module -Name PsOcr | Install-Module -Scope CurrentUser
-# Must be run in windows powershell so im launching the powershell i got in path which is ver 5
-powershell
-Import-Module -Name C:\Users\Nichita\Documents\PowerShell\Modules\PsOcr 
-Get-ChildItem -File -Filter *_z.png -Recurse | Convert-PsoImageToText | Select-Object Text | foreach {$_.Text}| ConvertTo-Json | Set-Clipboard 
-
-
 # Create the jsons for each augment
 function New-AugmentJson() {
     param (
         $directory
     )
-    Write-Host $directory;
-    $directory | Get-ChildItem -File -Filter *_icon.png | ForEach-Object {
-        $iconNameSplit = $($_.Name -split "_")
-        $properties = [ordered]@{
-            Id          = $iconNameSplit[0];
-            Name        = $($iconNameSplit[1]);
-            IconName    = $_.Name;
-            Description = @("");
+    $i = 1;
+    Write-Host $directory.FullName;
+    $directory | Get-ChildItem -File -Filter *.png | Sort-Object Name | ForEach-Object {
+        $i++;
+        $id = ([System.Math]::Floor($i / 2)).ToString();
+        Write-Host $_.FullName
+        if ("ULT" -eq $directory.Name -and 8 -eq $i) {
+            $name = "icon.png"
+            Rename-Item -Path $_.FullName -NewName $name
         }
-        return $properties # prevents overwrite, maybe should keep description text in a separate file and build it up into this json but  I see no value in it atm. inb4 i curse myself later
+        elseif ($i % 2 -eq 0) {
+            $name = $id + "_icon.png";
+            Rename-Item -Path $_.FullName -NewName $name
+        }
+        else {
+            $name = $id + "_z.png"
+            Rename-Item -Path $_.FullName -NewName $name
+            # The OCR module must be run in windows powershell so im launching the powershell i got in path which is ver 5
+            $x = powershell -OutputFormat Text -Command "& {
+            Import-Module -Name C:\Users\Nichita\Documents\PowerShell\Modules\PsOcr
+            Get-Item $(Join-Path $directory $($id + "_z.png")) | Convert-PsoImageToText | Select-Object Text | ForEach-Object { `$_.Text } 
+            }"
+            $properties = [ordered]@{
+                Id          = $id;
+                Name        = $x[0];
+                IconName    = $id + "_icon.png";
+                Description = @();
+            }
+            $desc = $x | Select-Object -Skip 1 
+            if ($desc[0].Contains("[")) {
+                $desc = $desc | Select-Object -Skip 1 
+            }
+            $properties.Description += $desc
+            return $properties # prevents overwrite, maybe should keep description text in a separate file and build it up into this json but  I see no value in it atm. inb4 i curse myself later
+        }
     }
 }
-
-function Get-AugmentDirectories {
-    param (
-        $directory
-    )
-    return $directory | Get-ChildItem -Directory -Recurse | Where-Object { $_ | Get-ChildItem -File -Filter *_icon.png | Select-Object -First 1 }
-}
-
-#single hero
-Get-AugmentDirectories -directory (Get-Location)| ForEach-Object { New-AugmentJson -directory $_.FullName }
-# root folder
-Get-AugmentDirectories -directory $(Join-Path (Get-Location)  '\Heroes\') | ForEach-Object { New-AugmentJson -directory $_.FullName }
-
-
-# script to add the iconpath argument that i forgot to add, facepalm. added iconpath to the new-augmentjson script above so this is largely unnecessary now 
-function Patch-AugmentJson() {
-    param (
-        $directory
-    )
-    Write-Host $directory;
-    $directory | Get-ChildItem -File -Filter *_icon.png | ForEach-Object {
-        $iconNameSplit = $($_ -split "_")
-        $fileName = $($iconNameSplit[0] + ".json")
-        $filePath = Join-Path $directory $fileName
-       
-        $o = ConvertFrom-Json (Get-Item $filePath | Get-Content -Raw )
-        $o | Add-Member -NotePropertyName IconName  -NotePropertyValue $_.Name
-        $json = ConvertTo-Json $o 
-        
-        $json | Out-File -FilePath $filePath 
-    }
-}
-Get-HeroAugmentDirectories -directory $(Get-Location) | ForEach-Object { Patch-AugmentJson $_.FullName }
-
-
 
 # Create a json for the hero and compile the hero's augments jsons into it
-
 function Get-HeroJson() {
     param (
         [Parameter(
@@ -123,8 +104,9 @@ function Get-HeroJson() {
                 if ($category.Name -eq "ULT") {
                     $augmentCategory = [ordered]@{
                         Type     = $category.Name;
-                        Contents = ,(New-AugmentJson $category)
+                        Contents = @()
                     }
+                    $augmentCategory.Contents += (New-AugmentJson $category)
                     $augmentCategory
                     continue;
                 }
@@ -138,8 +120,9 @@ function Get-HeroJson() {
                     $skillAugment = [ordered]@{
                         Id       = $subCategoryNameSplit[0]; 
                         Name     = $subCategoryNameSplit[1];
-                        Augments = ,(New-AugmentJson $subCategory)
+                        Augments = @()
                     }
+                    $skillAugment.Augments += New-AugmentJson $subCategory
                     $augmentCategory["Contents"] += $skillAugment
                 }
                 $augmentCategory
@@ -160,11 +143,6 @@ function Get-HeroJson() {
 
 #single hero 
 Get-Location | Get-HeroJson | ConvertTo-Json -Depth 10 |  Out-File 'HeroInfo.json'
-#all heroes
-Get-ChildItem -directory $(Join-Path (Get-Location)  '\Heroes\') | ForEach-Object {
-    $_ | Get-HeroJson | ConvertTo-Json -Depth 10 |  Out-File (Join-Path $_ 'HeroInfo.json')
-}
-    
 
 # Make a json linking to every hero json, yeah something feels a bit wrong. Ill refactor if im still working on this at least a week
 $array = $(Join-Path (Get-Location)  '\Heroes\') | Get-ChildItem -Directory | ForEach-Object { $_.Name }
